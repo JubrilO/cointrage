e985f4503437e8318548391b5e8d35fe7776bc71
moyo-paystack <moyo@paystack.com>
2021-01-03 18:58:34 +0100

    Revert "create list of exchanges"
    This reverts commit fbeb9e96a276f2d763283c237c03928c2b867a1a.

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
 from app.base_model import Base, engine
 from app.market import Market
-from app.exchange_list import ExchangeList
 
 Base.metadata.create_all(engine)
 
 if __name__ == "__main__":
     # luno = Luno()
     # luno.run()
-    ExchangeList.init_exchange()
     market = Market()
     market.run()
